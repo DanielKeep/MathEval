eb98eac48da9929d29fc8604c0c21319601d5ec9
Daniel Keep <daniel.keep@gmail.com>
2010-05-13 18:28:00 +1000

    Fixed build script.

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
-REM xfbuild -g -debug -unittest -version=Unittest +oLexerTest LexerTest.d
-xfbuild -g -debug -unittest -version=Unittest +oAstTest AstTest.d
+call dmdenv 1.057-tango-0.99.9
+xfbuild -g -debug -unittest -version=Unittest +D.xf/LexerTest.deps +O.xf/LexerTest.objs +oLexerTest LexerTest.d
+xfbuild -g -debug -unittest -version=Unittest +D.xf/AstTest.deps +O.xf/AstTest.objs +oAstTest AstTest.d
