589fbb77a4096b697073abd138ebc3e4c4d8f32a
Daniel Keep <daniel.keep@gmail.com>
2010-05-27 03:24:46 +1000

    Silenced build.cmd.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
+@ECHO OFF
 call dmdenv 1.057-tango-0.99.9
 xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/LexerTest.deps +O.xf/LexerTest.objs +oLexerTest LexerTest.d
 xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/AstTest.deps +O.xf/AstTest.objs +oAstTest AstTest.d
