cb5b8bc002d2d3ccc719fa6dd9cfb7429086131f
Daniel Keep <daniel.keep@gmail.com>
2010-05-29 13:19:49 +1000

    Build now only builds the REPL.

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 call dmdenv 1.057-tango-0.99.9
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/LexerTest.deps +O.xf/LexerTest.objs +oLexerTest LexerTest.d
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/AstTest.deps +O.xf/AstTest.objs +oAstTest AstTest.d
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/EvalTest.deps +O.xf/EvalTest.objs +oEvalTest EvalTest.d
-xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/ReplTest.deps +O.xf/ReplTest.objs +oReplTest ReplTest.d
+REM xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/LexerTest.deps +O.xf/LexerTest.objs +oLexerTest LexerTest.d
+REM xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/AstTest.deps +O.xf/AstTest.objs +oAstTest AstTest.d
+REM xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/EvalTest.deps +O.xf/EvalTest.objs +oEvalTest EvalTest.d
+xfbuild -g -debug -unittest -version=Unittest +xtango +D.xf/ReplTest.deps +O.xf/ReplTest.objs +oMathEval ReplTest.d
