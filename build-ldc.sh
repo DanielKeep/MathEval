a3d79d65a1d9e9b5bf62661b253961b80f6ddbbc
Daniel Keep <daniel.keep@gmail.com>
2010-06-08 19:51:18 +1000

    Fixed linux build for Mr. Complainy (bernardh).

--- build-ldc.sh
+++ build-ldc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-xfbuild +cldc -d-debug -unittest -d-version=Unittest +xtango +D.xf/Cli.deps +O.xf/Cli.objs +oMathEval -d-version=MathEval_Lists Cli.d
+xfbuild +cldc -d-debug +xtango +D.xf/GitVerGen.deps +O.xf/GitVerGen.objs +oGitVerGen GitVerGen.d
+./GitVerGen
+xfbuild +cldc -d-debug -unittest -d-version=Unittest +xtango +D.xf/Cli.deps +O.xf/Cli.objs +oMathEval -J=. -d-version=MathEval_Lists Cli.d
 
